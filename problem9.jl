a1987bab9500b7aa959a7b0b8acbeff2b6e03733
Markus Amano <markus.a.amano@gmail.com>
2024-09-10 14:28:46 +0900

    optimized 9

--- problem9.jl
+++ problem9.jl
@@ -5,12 +5,13 @@ end
 function solution(n = 1000)
     for a in 0:n
         for b in (a+1):n
-            c = isqrt(a^2 + b^2)
-            if  a + b + c == n && ispythagoreantriple(a, b, c)
+            c = n - a - b
+            if ispythagoreantriple(a, b, c)
                 return a*b*c
             end
         end
     end
+    return -1
 end
 
 println(solution())
