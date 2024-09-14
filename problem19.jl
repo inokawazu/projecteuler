622c9ba25576c97712c56bed1e2eaf3eb1c0fd23
Markus Amano <markus.a.amano@gmail.com>
2024-09-14 18:34:33 +0900

    made 19 "my own"

--- problem19.jl
+++ problem19.jl
@@ -1,9 +1,30 @@
 using Dates
 
-function solution(startyear=1901, endyear=2000)
-    count(Date(y, m, 1) for y in startyear:endyear, m in 1:12) do date
-        dayofweek(date) == 7
+isleap(y) = mod(y, 4) == 0 && (mod(y, 100) != 0 || mod(y, 400) == 0)
+leapoffset(y) = isleap(y) ? 1 : 0
+
+function solution()
+    startyear = 1901
+    endyear = 2000
+
+    monthdays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    sundayoffset = 0
+    nsundays = 0
+
+    for year in startyear:endyear
+        for (imonth, mdays) in enumerate(monthdays)
+            nsundays += iszero(sundayoffset)
+
+            sundayoffset += mdays
+            if imonth == 2
+                sundayoffset += leapoffset(year)
+            end
+            sundayoffset = mod(sundayoffset, 7)
+        end
     end
+
+    return nsundays
 end
 
 println(solution())
