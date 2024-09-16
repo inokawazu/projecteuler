bd7fc9380774bda545e260c20a3fe27a47e77cb9
Markus Amano <markus.a.amano@gmail.com>
2024-09-16 11:55:21 +0900

    optimized 28

--- problem28.jl
+++ problem28.jl
@@ -1,42 +1,10 @@
-struct SpiralNumbers end
+function solution(input::T) where T
+    nmax = (input - 1) ÷ 2
+    oddsquares = ((2n+1)^2 for n in 1:nmax)
 
-Base.iterate(_::SpiralNumbers) = ((0, 0), (0, 0, 0))
-
-function Base.iterate(_::SpiralNumbers, (layer, x, y))
-    l = layer
-
-    nl = layer
-    npos = (x, y)
-
-    nl += x == y == l ? 1 : 0
-
-    npos = (x, y) .+ 
-    if x == l && -l < y < l
-        (0, -1)
-    elseif -l < x <= l && y == -l
-        (-1, 0)
-    elseif x == -l && -l <= y < l
-        (0, 1)
-    elseif -l <= x <= l && y == l
-        (1, 0)
-    end
-
-    (npos, (nl, npos...))
-end
-
-function solution(input)
-    @assert isodd(input)
-    nlayers = (input - 1) ÷ 2
-    
-    spiralcoords = Iterators.takewhile(SpiralNumbers()) do coord
-        all(coord) do elem
-            abs(elem) <= nlayers
-        end
-    end
-
-    return sum(enumerate(spiralcoords)) do (i, (x, y))
-        abs(x) == abs(y) ? i : zero(i)
-    end
+    4 * sum(enumerate(oddsquares)) do (n, osq)
+        osq-3n
+    end + 1
 end
 
 const INPUT = 1001
