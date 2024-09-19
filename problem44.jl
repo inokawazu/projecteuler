c4783ddddda6b32e838bfb4ee9e103bdfc6cbea3
Markus Amano <markus.a.amano@gmail.com>
2024-09-19 09:48:21 +0900

    removed comments

--- problem44.jl
+++ problem44.jl
@@ -1,37 +1,10 @@
 pentagonal_number(n::Integer) = n * (3n - 1) ÷ 2
 
-# const PEN_CACHE = Set{Int}(pentagonal_number(n) for n in 1:1_000_000)
-# const PEN_CACHE = falses(pentagonal_number(1_000_000))
-# for n in 1:1_000_000
-#     PEN_CACHE[pentagonal_number(n)] = true
-# end
-
-# pentagonal_number(n) for n in 1:1_000_000
-
-
-# const NOPEN_CACHE = Set{Int}()
 function ispentagonal(n::Integer)
-    # return PEN_CACHE[n]
-    # return n in PEN_CACHE
-    # if n in PEN_CACHE
-    #     return true
-    # else
-    #     return false
-    # end
-
     nums = Iterators.countfrom(one(n))
     pents = Iterators.map(pentagonal_number, nums)
 
     return any(==(n), Iterators.takewhile(<=(n), pents))
-    # res = any(==(n), Iterators.takewhile(<=(n), pents))
-
-#     if res
-#         push!(PEN_CACHE, n)
-#     else
-#         push!(NOPEN_CACHE, n)
-#     end
-
-#     return res
 end
 
 function solution()
@@ -46,5 +19,4 @@ function solution()
     end
 end
 
-println(@time solution())
-println(@time solution())
+println(solution())
