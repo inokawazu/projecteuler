6bb6a9d315c1962230320930147272eed1e61e13
Markus Amano <markus.a.amano@gmail.com>
2024-09-28 14:40:04 +0900

    problem 60 done! ☻

--- problem60.jl
+++ problem60.jl
@@ -13,132 +13,45 @@ function find_prime_pairs(n::T) where T
     return output
 end
 
-# function get_nconnected_sets(pp, pp_graph, n)
-#     return get_nconnected_sets(Set(pp), pp_graph, n-1)
-# end
-
-function get_nconnected_sets(pp::T, pp_graph, n) where T
-    # if n == 0
-    #     return [pp_set]
-    # end
-
-    output = [Set(pp)]
-    n -= 1
-
-    while n > 0
-        pp_sets = copy(output)
-        empty!(output)
-
-        for pp_set in pp_sets
-            if any(pp->!(pp in keys(pp_graph)), pp_set)
-                continue
-            end
-
-            outwards = mapreduce(pp->pp_graph[pp], union, pp_set)
-            setdiff!(outwards, pp_set)
-
-            for outp in outwards
-                if !(outp in keys(pp_graph))
-                    continue
-                elseif !issubset(pp_set, pp_graph[outp]) 
-                    continue
-                elseif !all(pp->outp in pp_graph[pp], pp_set) 
-                    continue
-                end
-
-                cand_pp_set = union(pp_set, outp)
-                if !any(pps->issetequal(cand_pp_set, pps), output)
-                    push!(output, cand_pp_set)
-                end
-            end
+function is_prime_pair(n::T) where T
+    for id in 1:ndigits(n)-1
+        num1, num2 = divrem(n, 10^id)
+        ldnum2 = div(num2, 10^(id-1))
+        if !iszero(ldnum2) && all(isprime, (num1, num2))
+            return true
         end
-
-        n -= 1
     end
 
-    return output
-
-    # unique(issetequal,
-    # Iterators.flatmap(outwards) do outp
-    #     if !(outp in keys(pp_graph))
-    #         T[]
-    #     elseif !issubset(pp_set, pp_graph[outp]) 
-    #         T[]
-    #     elseif !all(pp->outp in pp_graph[pp], pp_set) 
-    #         T[]
-    #     else
-    #         get_nconnected_sets(union(pp_set, outp), pp_graph, n-1)
-    #     end
-    # end)
+    return false
 end
-
-# const MAX_REF = Ref(0)
-# function isnconnected(pp, pp_graph, n)
-    # if !(pp in keys(pp_graph))
-    #     return false
-    # end
-
-    # pp_set = Set(pp)
-    # union!(pp_set, pp_graph[pp])
-
-
-    # if (Set(( 3,7,109,673 )) ⊆ pp_set)
-    #     println(pp_set)
-    #     for pp in pp_set
-    #         if pp in keys(pp_graph)
-    #             println(pp, " => ", pp_graph[pp])
-    #         end
-    #     end
-    # end
-
-    # return (Set(( 3,7,109,673 )) ⊆ pp_set)
-
-    # filt_set = filter(pp_set) do opp
-    #     opp_cnt = count(pp_set) do oopp
-    #         opp == oopp ||
-    #         oopp in keys(pp_graph) && opp in pp_graph[oopp]
-    #     end
-    #     opp_cnt > 5
-    # end
-
-    # return length(filt_set) >= n
-
-    # intersect_set = mapreduce(intersect, pp_set) do opp
-    #     opp_set = Set(opp)
-    #     union!(opp_set, get(pp_graph, opp, empty(opp_set)))
-    #     if pp == 673 
-    #         println("DEBUG ", opp_set)
-    #     end
-    #     opp_set
-    # end
-    # return length(intersect_set) == n
-# end
 
 function solution(target::T = 5) where T
     nums = Iterators.countfrom(one(T))
+    nums = Iterators.take(nums, 100_000_000)
+
+    pp_primes = Set(Iterators.filter(x->is_prime_pair(x) && isprime(x), nums))
+
     primes = Iterators.filter(isprime, nums)
-    pp_graph = Dict{T, Set{T}}()
+    search_primes = collect(Iterators.take(primes, 10_000))
+    target -= 1
+    output = Set.(search_primes)
 
-    for prime in primes
-        if prime > 700_000
-            foreach(x->println(x, "=>", filter(<=(673), sort(collect(pp_graph[x])))), [3,7,109,673])
-            # @show isnconnected(673, pp_graph, target)
-            return
+    while target > 0
+        cats = Iterators.filter(Iterators.product(search_primes, output)) do (prime, pp_set)
+            !(prime in pp_set) && 
+            all(pp -> concateprod(prime, pp) in pp_primes && concateprod(pp, prime) in pp_primes, pp_set)
         end
 
-        for (ppl, ppr) in find_prime_pairs(prime)
-            pp_graph[ppl] = push!(get(pp_graph, ppl, Set{T}()), ppr)
-
-            ncsets = get_nconnected_sets(ppl, pp_graph, target)
-
-            if !isempty(ncsets)
-                println(ncsets)
-                return minimum(sum, ncsets)
-                # println(ppl," => ", pp_graph[ppl])
-                # return ppl + sum(pp_graph[ppl])
-            end
+        new_pp_sets = Iterators.map(cats) do (prime, pp_set)
+            union(pp_set, prime)
         end
+
+        output = unique(new_pp_sets)
+
+        target -= 1
     end
+
+    return minimum(sum, output)
 end
 
-println(solution(4))
+println(solution())
