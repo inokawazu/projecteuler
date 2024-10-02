4998ea4891ae1b403bf98b34ff9e49bcc7436a50
Markus Amano <markus.a.amano@gmail.com>
2024-10-02 15:49:55 +0900

    optimized problem 69

--- problem69.jl
+++ problem69.jl
@@ -1,56 +1,28 @@
-function pfactors(n::T) where T <: Integer
-    divs = Dict{T, T}()
+function phi(n::T) where T <: Integer
+    pro = one(T)
     maxi = isqrt(n)
 
-    for i in Iterators.countfrom(2one(T))
-        while mod(n, i) == 0
-            n ÷= i
-            divs[i] = get(divs, i, zero(T)) + 1
+    for p in Iterators.countfrom(2one(T))
+        k = zero(T)
+
+        while mod(n, p) == 0
+            n ÷= p
+            k += 1
         end
 
-        if isone(n)
+        if k > 0
+            pro *= p^(k-1) * (p - 1)
+        elseif isone(n)
             break
-        elseif i == maxi
-            divs[n] = get(divs, n, zero(T)) + 1
+        elseif p == maxi
+            pro *= (n - 1)
             n ÷= n
             break
         end
     end
-    return divs
+
+    return pro
 end
-
-function phi(n::Integer)
-    divs = pfactors(n)
-    prod(divs) do (p, k)
-        p^(k-1) * (p - 1)
-    end
-end
-
-# function phi(n::T) where T <: Integer
-#     pro = one(T)
-#     maxi = isqrt(n)
-
-#     for p in Iterators.countfrom(2one(T))
-#         k = zero(T)
-
-#         while mod(n, p) == 0
-#             n ÷= p
-#             k += 1
-#         end
-
-#         if k > 0
-#             pro *= p^(k-1) * (p - 1)
-#         elseif isone(n)
-#             break
-#         elseif p == maxi
-#             n ÷= n
-#             pro *= (n - 1)
-#             break
-#         end
-#     end
-
-#     return pro
-# end
 
 nphi(n) = n / phi(n)
 
