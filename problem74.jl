8781caefa38b76cf1b8719cab0d3b37a5e937d25
Markus Amano <markus.a.amano@gmail.com>
2024-10-07 13:10:29 +0900

    problem 74 optimized

--- problem74.jl
+++ problem74.jl
@@ -13,23 +13,29 @@ factorial_sum(n) = sum(factorial, eachdigit(n))
 function solution(target::T=1_000_000) where T
     sol_cnt = 0
     cnt_target = 60
+    cache = Dict{T, T}()
 
     nrts = Set{T}()
     for n in 1:target-1
-        n % 10000 == 0 && @show n
-
-        cnt = 0
         empty!(nrts)
+        cnt = 0
+        n_start = n
 
-        while !(n in nrts) && cnt <= cnt_target
+        while !(n in nrts) && !(n in keys(cache))
             cnt += 1
             push!(nrts, n)
             n = factorial_sum(n)
         end
 
+        if n in keys(cache)
+            cnt += cache[n]
+        end
+
         if cnt == cnt_target
             sol_cnt += 1
         end
+
+        cache[n_start] = cnt
     end
 
     return sol_cnt
