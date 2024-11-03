cc92524cf310c537f43ca25285be41232d5ff9c7
Markus Amano <markus.a.amano@gmail.com>
2024-11-03 20:50:26 +0900

    problem 95 optimized to be 500% times faster.

--- problem94.jl
+++ problem94.jl
@@ -1,7 +1,4 @@
 function isareaintegral(a, b)
-    # b * sqrt(4a^2 - b^2) / 4
-    # b * sqrt(d) / 4
-    # b * e / 4
     d = 4*a^2 - b^2
     e = isqrt(d)
     isqrt(d)^2 == d && mod(d*e, 4) == 0
@@ -10,7 +7,7 @@ end
 isoperimeter(a, b) = 2a + b
 
 function subsol(as, limit)
-    sol = eltype(as)
+    sol = zero(eltype(as))
     for a in as
         bm = a - 1
         bp = a + 1
@@ -29,22 +26,12 @@ function subsol(as, limit)
 end
 
 function solution(limit=1_000_000_000)
-    sol = 0
-    for a in 2:limit
-        bm = a - 1
-        bp = a + 1
-
-        if isoperimeter(a, bm) > limit
-            break
-        elseif isareaintegral(a, bm)
-            sol += isoperimeter(a, bm)
-        end
-
-        if isoperimeter(a, bp) <= limit && isareaintegral(a, bp)
-            sol += isoperimeter(a, bp)
-        end
+    nths = Threads.nthreads()
+    tsks = map(0:nths-1) do n
+        Threads.@spawn subsol(2+n:nths:limit, limit)
     end
-    return sol
+
+    return sum(fetch, tsks)
 end
 
 println(solution())
