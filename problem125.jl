a9969ca0a3dcff648901c0069bd2f5c95d0fa8b2
Markus Amano <markus.a.amano@gmail.com>
2025-04-26 09:48:52 +0900

    moved specialized function to own file

--- problem125.jl
+++ problem125.jl
@@ -1,13 +1,11 @@
 include("common.jl")
 
-function revidigits(n)
-    nthdigit(n, nth) = n ÷ 10^(nth - 1) % 10
-    Iterators.map(nth -> nthdigit(n, nth), ndigits(n):-1:1)
+function revidigits(n; base = 10)
+    Iterators.map(nth -> nthdigit(n, nth, base=base), ndigits(n, base=base):-1:1)
 end
 
 ispalindrome(n) = all(d1 == d2 for (d1, d2) in zip(idigits(n), revidigits(n)))
 
-# sumofsquares(ki, kf) = (kf*(1 + kf)*(1 + 2*kf) + ki*(-1 + (3 - 2*ki)*ki)) ÷ 6
 sumofsquares(l, n) = - ((-1 + l - n) * (-l + 2*l^2 + n+2*l*n + 2*n^2)) ÷ 6
 
 function solution(n = 10^8)
