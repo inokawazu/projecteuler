2d9e9739f09a3d89cd231cb4edf7156861d51801
Markus Amano <markus.a.amano@gmail.com>
2026-05-20 15:38:45 +0900

    make input the solution function arg

--- problem1.jl
+++ problem1.jl
@@ -2,8 +2,7 @@
 
 is35div(x) = iszero(mod(x, 3)) || iszero(mod(x, 5))
 
-function solution()
-    limit = 1000
+function solution(limit = 1000)
     nats = Iterators.countfrom()
     divnums = Iterators.filter(is35div, nats)
     belowlimit = Iterators.takewhile(<(limit), divnums)
